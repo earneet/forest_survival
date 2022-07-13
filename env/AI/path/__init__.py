d6f5aad2ccfd1daedb02e9d4b68e6674395b8e4a
earneet <earneet@gmail.com>
2022-07-13 09:41:53 +0000

    codespaces test

--- env/AI/path/__init__.py
+++ env/AI/path/__init__.py
@@ -1,9 +1,10 @@
-
 from .find_path import a_star
+
 
 find_path_solvers = {
     "a_star": a_star
 }
+
 
 def find_path(game_obj, start, end, world_map, algorithm="a_star"):
     return find_path_solvers[algorithm](game_obj, start, end, world_map)
